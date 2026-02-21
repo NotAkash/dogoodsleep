15ca8b0048d9a74fb877173b97ee400bdc4d0c05
NotAkash <20as166@queensu.ca>
2026-02-21 16:39:36 -0500

    css edit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,26 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { MainNav } from "@/components/MainNav";
+import "globals.css";
 
 const inter = Inter({ subsets: ["latin"], display: "swap" });
-export const runtime = 'edge';
+
 export const metadata: Metadata = {
-  title: "Place Y Face",
-  description: "Minimalist photography portfolio"
+    title: "do-good-sleep",
+    description: "Akash was here? i guess"
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <MainNav />
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <MainNav />
+                {children}
+            </body>
+        </html>
+    );
 }
